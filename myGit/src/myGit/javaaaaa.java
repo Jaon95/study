ca93a4f65b4f183f0ae44e745a24534fe18c4cfd
Jaon05 <jasonlmh0314@outlook.com>
2015-12-28 22:44:55 +0800

    "a change from test branch"

---
+++ myGit/src/myGit/javaaaaa.java
@@ -0,0 +1,10 @@
+package myGit;
+
+public class javaaaaa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i have create a new file ready to emerge");
+	}
+
+}
